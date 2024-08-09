1110c1a2c37c38c6e85a4a9e8425c24cabe9ab21
kieranp-p6 <kiip96@icloud.com>
2024-08-09 11:24:47 +0100

    removed example from js file

--- twoSum.js
+++ twoSum.js
@@ -1,15 +1,9 @@
-/**
- * Hello challenger! Your task is to write a function named `reverseArray` which takes in a array and returns it reversed
- *
- * THE ARRAY CAN CONTAIN ANY DATATYPE BUT ONLY USE NUMBERS
- *
- * [2, 7, 11, 15], Target number = 9    should give an Output: [2, 7]
- * [3, 2, 4], Target number = 6         should give an Output: [2, 4]
- * [3, 3], Target number = 6            should give an Output: [3, 3]
- * [1, 2, 3, 4], Target number = 8      should give an Output: null
- */
-
-
 export const twoSum = (array, target) => {
 
 }
+
+// You can use these as a dummy array choose 1 delete the others
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [2, 7]
+console.log(twoSum([3, 2, 4], 6)); // Output: [2, 4]
+console.log(twoSum([3, 3], 6)); // Output: [3, 3]
+console.log(twoSum([1, 2, 3], 7)); // Output: null
