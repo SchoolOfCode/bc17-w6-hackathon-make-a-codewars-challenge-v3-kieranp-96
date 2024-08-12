0fad9dc0fc5eef622440ce6b5bc3456934eea00c
kieranp-p6 <kiip96@icloud.com>
2024-08-12 07:20:37 +0100

    test

---
+++ reverse.js
@@ -0,0 +1,19 @@
+/**
+ * FIRST THINGS FIRST REMOVE .SKIP FROM THE reverse.test.js file AND add .skip to the twoSum.test.js
+ *
+ *
+ *
+ * Hello challenger! Your task is to write the logic for `reverseArray` that will take an array and returns it reversed array
+ * The array can contain any datatype
+ * The array can contain any datatype
+ * The array can contain any datatype
+ *
+ * A few examples:
+ *
+ *    [true, false, null, undefined] should return [undefined, null, false, true]
+ *     [1,2,3,4,5] should return [5,4,3,2,1]
+ */
+
+export const reverseArray = (array) => {
+
+}
